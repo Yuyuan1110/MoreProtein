3c207ca73bf39657a95b2a70016186f45410221e
Yuyuan <z0975750027@gmail.com>
2024-06-14 22:24:36 +0800

    server can running but can't visit.

--- src/main/java/com/moreprotein/MoreProteinApplication.java
+++ src/main/java/com/moreprotein/MoreProteinApplication.java
@@ -2,11 +2,8 @@ package com.moreprotein;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.cloud.config.server.config.ConfigServerMvcConfiguration;
 
-@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class, ConfigServerMvcConfiguration.class, DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class MoreProteinApplication {
     public static void main(String[] args) {
         SpringApplication.run(MoreProteinApplication.class, args);
